38e41db83b646625f7811f17b01af1b8bf5e942b
YunYouJun <me@yunyoujun.cn>
2022-04-25 21:52:29 +0800

    feat: add toast tip for 200 but no data, close #13

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -3,6 +3,12 @@ import { $fetch } from 'ohmyfetch'
 export const apiFetch = $fetch.create({
   baseURL: import.meta.env.VITE_API_URL,
   retry: 0,
+  async onResponse({ response }) {
+    const { useToast } = await import('vue-toastification')
+    const toast = useToast()
+    if (typeof response._data === 'string')
+      toast.error('坏了！服务器坏了！快去打莉沫酱！')
+  },
   async onResponseError({ response }) {
     const { useToast } = await import('vue-toastification')
     const toast = useToast()
