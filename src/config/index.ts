d8f1a480f8e4aa1d3bef34814041c5ffa6752a11
YunYouJun <me@yunyoujun.cn>
2022-11-01 00:32:20 +0800

    chore: fix rimo-and-xiao-yun banner url to local

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
-export const bannerUrl = 'https://upyun.yunyoujun.cn/images/sese-rimo-and-xiao-yun.png'
+// export const bannerUrl = 'https://r2.yunyoujun.cn/images/sese-rimo-and-xiao-yun.png'
+export const bannerUrl = '/sese-rimo-and-xiao-yun.png'
