3ba9dc0927b304db7eae6d9af63cab86165cb1d1
YunYouJun <me@yunyoujun.cn>
2022-11-25 02:32:14 +0800

    feat: add opensearch.xml & upgrade deps

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,27 @@
 import { isDark } from '~/composables'
 
 const { t } = useI18n()
+const title = computed(() => t('sese.title'))
 
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+
 useHead({
-  title: computed(() => t('sese.title')),
+  title,
   meta: [
     { name: 'description', content: 'SeSe Engine 是一个轻量级的搜索引擎，可以轻松部署在自己的个人服务器上。' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'search',
+      type: 'application/opensearchdescription+xml',
+      title,
+      href: '/opensearch.xml',
     },
   ],
 })
